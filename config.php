4f31f6ad28c2a2c78f2e52a318c63ea9cf6b788e
petronick <petro980@gmail.com>
2018-01-04 22:10:39 +0100

    change of config file

--- config.php
+++ config.php
@@ -1,9 +1,4 @@
 <?php
-define("DB","db_vama");
-define("DBHOST","localhost");
-define("DBUSER","root");
-define("DBPASS","");
-define("HB","mysql:localhost;db_vama");
 define("APP_DIR","d:/wamp/www/IBC");
 
 function __autoload($class){
